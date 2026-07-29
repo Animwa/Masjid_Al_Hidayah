961de9ce50260ab278fe25ee62e24192b435ee5b
arifin05 <107057460+Animwa@users.noreply.github.com>
2026-07-29 17:21:07 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // FRONTEND LOGIC & INTEGRASI API WEB MASJID AL HIDAYAH (FIXED BLANK SCREEN)
 // ==========================================
 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbykwykXc-BsBAo7k-tpbxLJBExbFXnODdhR6Ofz2SptM-lggZPlUfXY_Bf-yLK2FjgBzg/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyoG4tELzEtUBHRgAiefYgMiVB16xrMnPb8xBOXXzs4XFuKVvvqHXcpues0CWk1SbLqRA/exec";
 
 let appData = {
   pengurus: [],
